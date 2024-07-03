461abf770cf4a423ca6cb636d9f3aba4d7873b93
Andy Chang <andy.chang@gmail.com>
2024-07-03 23:04:43 +0800

    fix: document not defined in server

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,15 +10,18 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
-  return ReactDOM.createPortal(
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div>
-      <div className="absolute bottom-0 sm:static bg-white rounded-lg p-4 z-10 max-w-full sm:max-w-md mx-auto w-full" onClick={(e) => e.stopPropagation()}>
-        {children}
-      </div>
-    </div>,
-    document.body
-  );
+  if (typeof window === "object") {
+    return ReactDOM.createPortal(
+      <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div>
+        <div className="absolute bottom-0 sm:static bg-white rounded-lg p-4 z-10 max-w-full sm:max-w-md mx-auto w-full" onClick={(e) => e.stopPropagation()}>
+          {children}
+        </div>
+      </div>,
+      document.body
+    );
+  }
+  return null
 };
 
 export default Modal;
